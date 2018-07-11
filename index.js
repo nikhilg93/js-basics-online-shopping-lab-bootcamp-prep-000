b9fc017ecdd77267705a46f7272ba452f0af91b0
Nikhil Gangrade <nikhil12gangrade93@yahoo.com>
2018-07-11 21:10:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -73,8 +73,7 @@ function placeOrder(cardNumber) {
     return ("Sorry, we don't have a credit card on file for you.")
   }
    else{
-    cart =[];
     return(`Your total cost is $${totalPriceItem}, which will be charged to the card ${cardNumber}.`)
-    
+    cart = [];
 }
 }
