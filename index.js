8957830bb8b1b5a42c3e89de846c1a212919fb94
Nikhil Gangrade <nikhil12gangrade93@yahoo.com>
2018-07-11 21:10:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
-  //var totalPriceItem = total();
+  var totalPriceItem = total();
   console.log()
   if(!cardNumber) {
     return ("Sorry, we don't have a credit card on file for you.")
